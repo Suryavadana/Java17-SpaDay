ef35eafee4b9410d782bf6891ba3859935da5cc5
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-05-09 20:25:24 -0500

    User credentials

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,35 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "user")
+public class UserController {
+
+    // /user/add
+    @GetMapping("add")
+    public String displayAddUserForm(){
+        return"user/add";
+
+    }
+
+    @PostMapping("")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+       if(verify.equals(user.getPassword( ))){
+            return "user/index";
+       }
+       else{
+           model.addAttribute("username", user.getUsername());
+           model.addAttribute("email",user.getEmail());
+           model.addAttribute("error", "Incorrect Password");
+           return "user/add";
+       }
+    }
+
+}
